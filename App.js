c8b3d798cb5f26db64722125d4560b2fc569a6a0
Jesse Gibson <Jesse_Gibson@me.com>
2017-05-07 14:01:35 -0600

    Add eslint
    All manner of fancy eslint plugins. This time I'm trying it in yaml and
    it looks *beautiful*.

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <StatusBar hidden />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
