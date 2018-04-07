c52b60097ca96f559826391acec48f365c20c689
Jesse Gibson <Jesse_Gibson@me.com>
2018-04-07 15:28:09 -0600

    Fix navbar height offset
    Seems `react-navigation` thought adding a default statusbar offset was a
    good idea. This fixes it by subtracting that offset.

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import React from 'react';
 
+// eslint-disable-next-line import/default
+import Expo from 'expo';
+
 import LayoutManager from './src/components/LayoutManager';
 import LayoutConfig from './src/components/LayoutConfig';
 import ServerLink from './src/components/ServerLink';
@@ -18,14 +21,20 @@ const styles = StyleSheet.create({
 });
 
 const navigationOptions = {
-  headerStyle: { backgroundColor: colors.navbar.bg },
   headerTitleStyle: { color: colors.navbar.text },
   headerTintColor: colors.navbar.text,
+  headerStyle: {
+    marginTop: -Expo.Constants.statusBarHeight,
+    backgroundColor: colors.navbar.bg,
+  },
 };
 
 const Routes = StackNavigator(
   {
-    LayoutManager: { screen: LayoutManager, navigationOptions },
+    LayoutManager: {
+      screen: LayoutManager,
+      navigationOptions,
+    },
     LayoutConfig: { screen: LayoutConfig, navigationOptions },
     ServerLink: { screen: ServerLink, navigationOptions },
     Loading: { screen: Loading, navigationOptions },
