172402ca21c90e25b053091cd82671a8cb434ffe
Jesse Gibson <Jesse_Gibson@me.com>
2018-04-08 15:30:15 -0600

    Refetch groups periodically in dashboard mode
    Run a query occasionally if in dashboard mode. Fetches all groups and
    their on state. This is pretty expensive though, so it's a short term
    fix until I get gql subscriptions configured. Also, turns out the
    network is horribly unreliable on mobile. Maybe I've misconfigured
    something, but that'd be a weird bug.

--- src/components/DashboardDetector.js
+++ src/components/DashboardDetector.js
@@ -7,12 +7,54 @@ import R from 'ramda';
 import { KeepAwake } from 'expo';
 
 import { DASHBOARD_MODE } from '../reducers/switches';
+import * as actions from '../actions/groups';
 import { selector } from '../utils/redux';
 
-// For tablets, keep the screen on for eternity.
+// For tablets, keep the screen on forever. Fetch periodically.
 export class DashboardDetector extends React.Component {
   static propTypes = {
+    fetchAllGroups: PropTypes.func.isRequired,
     enabled: PropTypes.bool.isRequired,
+    serverUrl: PropTypes.string,
+  };
+
+  mounted = false;
+  timeout = null;
+
+  componentDidMount() {
+    this.mounted = true;
+    this.scheduleNextRefetch();
+  }
+
+  componentDidUpdate(prev) {
+    if (!prev.enabled && this.props.enabled) {
+      this.scheduleNextRefetch();
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+    this.mounted = false;
+  }
+
+  scheduleNextRefetch = () => {
+    // Possible to unmount while a request is in-flight.
+    if (!this.mounted || !this.props.enabled) {
+      return;
+    }
+
+    this.timeout = setTimeout(this.refetchGroups, 1000 * 10);
+  };
+
+  refetchGroups = () => {
+    // Switch disabled since it last scheduled.
+    if (!this.props.enabled) {
+      return;
+    }
+
+    this.props
+      .fetchAllGroups(this.props.serverUrl)
+      .then(this.scheduleNextRefetch, this.scheduleNextRefetch);
   };
 
   render() {
@@ -22,6 +64,11 @@ export class DashboardDetector extends React.Component {
 
 export const mapStateToProps = selector({
   enabled: R.path(['switches', DASHBOARD_MODE]),
+  serverUrl: R.path(['server', 'url']),
 });
 
-export default connect(mapStateToProps)(DashboardDetector);
+const mapDispatchToProps = {
+  fetchAllGroups: actions.fetchAllGroups,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardDetector);
--- src/components/__tests__/DashboardDetector.test.js
+++ src/components/__tests__/DashboardDetector.test.js
@@ -8,9 +8,13 @@ import { KeepAwake } from 'expo';
 import { DashboardDetector, mapStateToProps } from '../DashboardDetector';
 import { DASHBOARD_MODE } from '../../reducers/switches';
 
+jest.useFakeTimers();
+
 describe('DashboardDetector', () => {
   const setup = merge => {
     const props = {
+      serverUrl: 'http://filament.server',
+      fetchAllGroups: jest.fn().mockReturnValue(Promise.resolve()),
       enabled: true,
       ...merge,
     };
@@ -37,6 +41,73 @@ describe('DashboardDetector', () => {
     const keepawake = output.find(KeepAwake);
 
     expect(keepawake.exists()).toBe(false);
+  });
+
+  it('refetches light state periodically', () => {
+    const { props } = setup();
+
+    expect(props.fetchAllGroups).not.toHaveBeenCalled();
+    jest.runOnlyPendingTimers();
+    expect(props.fetchAllGroups).toHaveBeenCalledWith(props.serverUrl);
+  });
+
+  it('does not refetch if dashboard mode is disabled', () => {
+    const { props } = setup({ enabled: false });
+
+    jest.runOnlyPendingTimers();
+    expect(props.fetchAllGroups).not.toHaveBeenCalled();
+  });
+
+  it('self-schedules following refetches', () => {
+    const { props } = setup();
+    props.fetchAllGroups.mockReturnValue({
+      then: cb => cb(),
+    });
+
+    jest.runOnlyPendingTimers();
+    jest.runOnlyPendingTimers();
+
+    expect(props.fetchAllGroups).toHaveBeenCalledTimes(2);
+  });
+
+  it('continues to fetch automatically after failed fetches', () => {
+    const { props } = setup();
+    props.fetchAllGroups.mockReturnValue({
+      then: (done, catcher) => catcher(),
+    });
+
+    jest.runOnlyPendingTimers();
+    jest.runOnlyPendingTimers();
+
+    expect(props.fetchAllGroups).toHaveBeenCalledTimes(2);
+  });
+
+  it('stops fetching after unmounting', () => {
+    const { output, props } = setup();
+    output.instance().componentWillUnmount();
+
+    jest.runOnlyPendingTimers();
+
+    expect(props.fetchAllGroups).not.toHaveBeenCalled();
+  });
+
+  // Because apparently react-navigation NEVER UNMOUNTS!
+  it('starts refetching if enabled while mounted', () => {
+    const { output, props } = setup({ enabled: false });
+
+    output.setProps({ enabled: true });
+    jest.runOnlyPendingTimers();
+
+    expect(props.fetchAllGroups).toHaveBeenCalled();
+  });
+
+  it('stops refetching if disabled while mounted', () => {
+    const { output, props } = setup();
+
+    output.setProps({ enabled: false });
+    jest.runOnlyPendingTimers();
+
+    expect(props.fetchAllGroups).not.toHaveBeenCalled();
   });
 
   describe('mapStateToProps', () => {
